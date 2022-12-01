8161f4b97cb2c5c6cfc331ef6ba7284f2836e0cc
Steven Realpe <nutritionmegastorepalmira@gmail.com>
2022-12-01 10:54:44 -0500

    2022-12-01 10:54

--- global/core.php
+++ global/core.php
@@ -7,18 +7,10 @@ date_default_timezone_set('America/Bogota');
 // const URL_VISTA="https://mulata.fit/public/";
 // const URL_BLOG="https://mulata.fit/blog/";
 
-// echo $_SERVER['DOCUMENT_ROOT'];
-// $PATH_UPLOAD=$_SERVER['DOCUMENT_ROOT'].'/mulata.fit/public/uploads/';
+
 const URL_SERVER="http://localhost:88/";
 const URL_VISTA="http://localhost:88/public/";
 const URL_BLOG="http://localhost:88/blog/";
 
-// const URL_SERVER="http://172.16.2.172/mulata.fit/";
-// const URL_VISTA="http://172.16.2.172/mulata.fit/public/";
-// const URL_BLOG="http://172.16.2.172/mulata.fit/blog/";
-
-// const URL_SERVER="http://192.168.80.15/mulata.fit/";
-// const URL_VISTA="http://192.168.80.15/mulata.fit/public/";
-// const URL_BLOG="http://192.168.80.15/mulata.fit/blog/";
 
 
